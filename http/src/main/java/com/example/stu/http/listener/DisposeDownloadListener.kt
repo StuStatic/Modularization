35dd35a8f4ff3351313173faa96dec3db79100e7
chengpiyan <chengpiyan@meishubao.com>
2021-04-07 20:59:56 +0800

    删除无用module(http+imageloader)，添加module_arouter

--- http/src/main/java/com/example/stu/http/listener/DisposeDownloadListener.kt
+++
@@ -1,9 +0,0 @@
-package com.example.stu.http.listener
-
-/**
- * Created by stu on 2021/1/25.
- *
- */
-open interface DisposeDownloadListener : DisposeDataListener {
-    fun onProgress(progrss: Int)
-}
--- http/src/main/java/com/example/stu/http/listener/DisposeHandleCookieListener.kt
+++
@@ -1,9 +0,0 @@
-package com.example.stu.http.listener
-
-/**
- * Created by stu on 2021/1/25.
- *
- */
-open interface DisposeHandleCookieListener : DisposeDataListener {
-    fun onCookie(cookieStrLists: ArrayList<String?>?)
-}
